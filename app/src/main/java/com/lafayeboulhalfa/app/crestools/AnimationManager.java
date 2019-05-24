6ec25ecf7c596e16eaf5ebf2900e8a57e0faa21b
robertPatainson <50960158+robertPatainson@users.noreply.github.com>
2019-05-24 14:39:16 +0200

    Test Commit sur projet partager

--- app/src/main/java/com/lafayeboulhalfa/app/crestools/AnimationManager.java
+++ app/src/main/java/com/lafayeboulhalfa/app/crestools/AnimationManager.java
@@ -22,7 +22,6 @@ public class AnimationManager extends Animation {
     @Override
     protected void applyTransformation(float interpolatedTime, Transformation t) {
         int newHeight = (int) (startHeight + targetHeight * interpolatedTime);
-        //to support decent animation, change new heigt as Nico S. recommended in comments
         //int newHeight = (int) (startHeight+(targetHeight - startHeight) * interpolatedTime);
         view.getLayoutParams().height = newHeight;
         view.requestLayout();
